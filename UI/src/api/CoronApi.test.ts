86a9bca41fe5b1e765745109148e0bb92bb71791
isabelcooper <isabel@triptease.com>
2020-04-20 16:17:31 +0100

    fix(UI): hide offending chai file to focus on db changes

--- UI/src/api/CoronApi.test.ts
+++ UI/src/api/CoronApi.test.ts
@@ -1,7 +1,7 @@
-import {expect} from "chai";
-
-describe('Placeholder test', () => {
-  it('should pass', async () => {
-    expect(1).to.eql(1);
-  });
-});
+// import {expect} from "chai";
+//
+// describe('Placeholder test', () => {
+//   it('should pass', async () => {
+//     expect(1).to.eql(1);
+//   });
+// });
