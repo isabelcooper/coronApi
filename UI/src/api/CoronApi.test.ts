cd8c7a65d7ac8c80ad197f8faf2d2016318d3d9d
isabelcooper <isabel@triptease.com>
2020-04-21 15:17:02 +0100

    feat(api-abstraction): get all statuses

--- UI/src/api/CoronApi.test.ts
+++ UI/src/api/CoronApi.test.ts
@@ -1,7 +1,55 @@
 import {expect} from "chai";
+import {CoronApi} from "./CoronApi";
+import {buildStatus, Status} from "../../../shared/Status";
+import {Random} from "../../../API/utils/Random";
 
-describe('Placeholder test', () => {
-  it('should pass', async () => {
-    expect(1).to.eql(1);
+function buildRandomSetOfStatuses(rows? :number): Status[] {
+  let i: number;
+  const jsonRota = [];
+  for (i = 0; i <= (rows || Random.integer(20)); i++) {
+    jsonRota.push(buildStatus())
+  }
+  return jsonRota;
+}
+
+describe('CoronApi', () => {
+  it('gets transformed data', async () => {
+    const statuses = buildRandomSetOfStatuses();
+
+    const workingFetch = async (_input: RequestInfo, _options?: RequestInit): Promise<Response> => {
+      async function getText() {
+        return JSON.stringify({statuses});
+      }
+      return {status: 200, text: getText} as Response;
+    };
+    const coronApi = new CoronApi('', workingFetch);
+    const response = await coronApi.getAllStatuses();
+    expect(response.statuses.length).to.eql(statuses.length);
+    expect(response.statuses[0].country).to.eql(statuses[0].country);
+    expect(response.statuses[0].flightStatus).to.eql(statuses[0].flightStatus);
+    //TODO: improve dates to deep equal in formatting
+    // expect(response.statuses).to.eql(statuses);
   });
+
+  it('returns errors from the api', async () => {
+    const errorFetch = async (_input: RequestInfo, _options?: RequestInit): Promise<Response> => {
+      async function getText() {
+        return JSON.stringify({error: 'error getting data from db'});
+      }
+      return { status: 500, text: getText } as Response;
+    };
+    const rotaApi = new CoronApi('', errorFetch);
+    const rota = await rotaApi.getAllStatuses();
+    expect(rota).to.eql({statuses: [], error: 'error getting data from db'})
+  });
+
+  it('handles fetch errors', async() => {
+    const failedFetch = async (_input: RequestInfo, _options?: RequestInit): Promise<Response> => {
+      throw new Error('failed to fetch')
+    };
+    const surveyApi = new CoronApi('', failedFetch);
+    const rotaRows = await surveyApi.getAllStatuses();
+    expect(rotaRows).to.eql({statuses: [], error: 'failed to fetch'})
+  });
+
 });
