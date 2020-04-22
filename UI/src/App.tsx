499c1e4614269db9a87b25f3111fe1a5932ec583
isabelcooper <isabel@triptease.com>
2020-04-22 13:35:55 +0100

    feat(react): testing tsconfig manipulation

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import {React, PureComponent} from 'react';
 import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import styled from "styled-components";
+import {CoronApiHttpClient} from "./api/CoronApiHttpClient";
 
 const Layout = styled.div`
   max-width: 500px;
@@ -11,14 +12,29 @@ const Layout = styled.div`
 function App() {
   // const api = new CoronApi();
   return (
-      <BrowserRouter>
-        <Route exact path={'/'}>
-          <Layout>
-            <h1>TITLE</h1>
-          </Layout>
-        </Route>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Route exact path={'/'}>
+        <Layout>
+          <h1>TITLE</h1>
+          {/*<StatusList/>*/}
+        </Layout>
+      </Route>
+    </BrowserRouter>
   );
 }
+//
+// class StatusList extends PureComponent {
+//   async componentDidMount() {
+//     const apiClient = new CoronApiHttpClient('http://localhost:1010');
+//     const statuses = await apiClient.getAllStatuses();
+//     console.log("*********", statuses);
+//   }
+//
+//  render() {
+//    return (
+//            <h1>Statuslist</h1>
+//    );
+//  }
+// }
 
 export default App;
