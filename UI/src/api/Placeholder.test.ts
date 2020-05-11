26e2156b6a12c2efba6687812891b82935eca465
isabelcooper <isabel@triptease.com>
2020-05-11 15:34:28 +0100

    fix(ui): comment out useless test for now

--- UI/src/api/Placeholder.test.ts
+++ UI/src/api/Placeholder.test.ts
@@ -1,7 +1,7 @@
-import {expect} from "chai";
-
-describe('Placeholder test', () => {
-  it('should always pass', async () => {
-    expect(1).to.eql(1);
-  });
-});
+// import {expect} from "chai";
+//
+// describe('Placeholder test', () => {
+//   it('should always pass', async () => {
+//     expect(1).to.eql(1);
+//   });
+// });
