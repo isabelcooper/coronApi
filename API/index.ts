4208df3019a584ee367ff0aa248d5e2802b59e90
isabelcooper <isabel@triptease.com>
2020-05-19 14:35:25 +0100

    feat(auth): wire basic auth in to server start in index (uses .env)

--- API/index.ts
+++ API/index.ts
@@ -1,10 +1,16 @@
 import {Server} from "./src/server";
 import {TravelStatusStorageHandler} from "./src/storageHandlers/TravelStatusStorageHandler";
-import {InMemoryTravelStatusReader, InMemoryTravelStatusWriter, StatusReader, StatusWriter} from "./src/travelStatusStore/StatusStore";
+import {StatusReader, StatusWriter} from "./src/travelStatusStore/StatusStore";
 import {TravelStatusRetrievalHandler} from "./src/retrievalHandlers/TravelStatusRetrievalHandler";
 import {TravelBansStorageHandler} from "./src/storageHandlers/TravelBansStorageHandler";
+import {BasicAuthAuthenticator} from "./src/auth/Authenticator";
+require('dotenv').config();
 
 export async function start(statusWriter: StatusWriter, statusReader: StatusReader) : Promise<void> {
-  const server = new Server(new TravelStatusRetrievalHandler(statusReader), new TravelStatusStorageHandler(statusWriter), new TravelBansStorageHandler(statusWriter), "");
+  const credentials = {
+    username: process.env.USERNAME as string,
+    password: process.env.PASSWORD as string,
+  };
+  const server = new Server(new TravelStatusRetrievalHandler(statusReader), new TravelStatusStorageHandler(statusWriter), new TravelBansStorageHandler(statusWriter), new BasicAuthAuthenticator(credentials));
   server.start();
 }
